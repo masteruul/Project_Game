14737d6bca78d812cf789477a9a556131ec359dc
Muhamad Syamsul Huda <syamsulhuda.uul@gmail.com>
2017-11-30 08:30:02 +0700

    Initial commit

---
+++ submit1/export/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1503564702,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/pesawat-sheet0.png",
+		"images/peluru-sheet0.png",
+		"images/meteor-sheet0.png",
+		"images/ledakan_meteor.png",
+		"images/ledakan_pesawat.png",
+		"images/bg_score-sheet0.png",
+		"images/game_over-sheet0.png",
+		"images/background-sheet0.png",
+		"media/sounddestroy.ogg",
+		"media/soundpeluru.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
